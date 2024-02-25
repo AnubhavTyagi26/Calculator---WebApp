4c9a950ba9717b091c0ffedb9b5d254ba12e8d11
AnubhavTyagi26 <anubhavtyagi269@gmail.com>
2024-02-25 13:25:32 +0530

    Calculator

---
+++ index.js
@@ -0,0 +1,23 @@
+//program
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function DeleteText(){
+    display.value = display.value.substring(0 , display.value.length - 1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
